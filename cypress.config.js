ccef9264d81bde622476ebeb922262e6eabd11e8
gumg90 <gumg90@gmail.com>
2023-07-20 22:14:26 -0300

    workflow

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,11 @@ require("dotenv").config()
 
 module.exports = defineConfig({
   reporter: 'mochawesome',
+  "reporterOptions": {
+    "overwrite": false,
+    "html": false,
+    "json": true
+  },
   e2e: {
     baseUrl: context[process.env.CONTEXT].baseUrl,
     specPattern: `**/${process.env.CONTEXT}/*.cy.js`,
