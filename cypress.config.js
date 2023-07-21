015b07079480e0b06028e311a3cbbea723ee9652
gumg90 <gumg90@gmail.com>
2023-07-20 22:17:32 -0300

    workflow

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 
 module.exports = defineConfig({
   reporter: 'mochawesome',
-  "reporterOptions": {
+  reporterOptions: {
     "overwrite": false,
     "html": false,
     "json": true
@@ -15,7 +15,7 @@ module.exports = defineConfig({
     video: false,
     screenshotOnRunFailure: true,
     async setupNodeEvents(on, config) {
-      return require("./config/plugin")(on, config);
+      return config;
     },
   },
 });
